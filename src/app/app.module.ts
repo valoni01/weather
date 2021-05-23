4c3ea1eb6829b96fc85f6b9362de4b7ac27f5039
valoni01 <valentine.awe@globalaccelerex.com>
2021-05-23 15:09:41 +0100

    app folder structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { ConvertTempPipe } from 'src/shared/pipes/convert-temp.pipe';
-import { ReqInterceptor } from 'src/core/req-interceptor';
 import { WeatherListComponent } from './weather/weather-list/weather-list.component';
 import { WeatherForecastComponent } from './weather/weather-forecast/weather-forecast.component';
+import { ReqInterceptor } from './core/req-interceptor';
+import { ConvertTempPipe } from './shared/pipes/convert-temp.pipe';
 
 @NgModule({
   declarations: [
--- src/app/core/req-interceptor.ts
+++ src/app/core/req-interceptor.ts
@@ -11,7 +11,7 @@ import {
 
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
-import { environment } from '../environments/environment'
+import { environment } from '../../environments/environment'
 
 
 @Injectable()
